60d77cd3eeaeb0c829a04c527ce40d39edc4d21a
Ronan Barzic <rbarzic@gmail.com>
2015-03-06 20:22:38 +0100

    Add mutex

--- async_lib/mutex.v
+++ async_lib/mutex.v
@@ -1,3 +1,7 @@
+// simple model of a mutex element
+// the random response time in case of conflict is not
+// modelized
+
 module mutex (/*AUTOARG*/
   // Outputs
   g1, g2,
@@ -5,48 +9,48 @@ module mutex (/*AUTOARG*/
   r1, r2
   );
   parameter DELAY = 1;
-  
+
   input r1;
   input r2;
   output g1;
   output g2;
 
   reg    g1_i,g2_i;
-  
+
   initial begin
     g1_i <= 0;
     g2_i <= 0;
   end
-  
-  
+
+
 
   always @* begin
     if(r1 && !g2 && !g2_i) begin
       g1_i <= 1'b1;
       g2_i <= 1'b0;
-      
+
     end
     else begin
       if(r2 && !g1 && !g1_i) begin
       g1_i <= 1'b0;
-      g2_i <= 1'b1;      
+      g2_i <= 1'b1;
       end
       else begin
         g1_i <= 1'b0;
-        g2_i <= 1'b0;      
+        g2_i <= 1'b0;
       end
-    end      
+    end
   end
-  
+
   assign #DELAY g1 = g1_i;
   assign #DELAY g2 = g2_i;
-  
 
 
-  
-   
+
+
+
 endmodule // mutex
-/*  
+/*
  Local Variables:
  verilog-library-directories:(
  "."
--- tb.v
+++ tb.v
@@ -8,11 +8,24 @@ module tb;
    reg cm0_b;
 
 
-   cmuller CM0(.z(cm0_z),
+   reg mut0_r1;
+   reg mut0_r2;
+
+
+   cmuller U_CM0(.z(cm0_z),
                .a(cm0_a),
                .b(cm0_b),
                .rstn(rst_async_)
                );
+
+
+   mutex U_MUTEX0(
+                  .g1(mut0_g1),
+                  .g2(mut0_g2),
+                  .r1(mut0_r1),
+                  .r2(mut0_r2)
+                  );
+
 
 
   // Dump all nets to a vcd file called tb.vcd
@@ -21,25 +34,20 @@ module tb;
 	$dumpfile("tb.vcd");
 	$dumpvars(0,tb);
      end
-
-
-
-
-
-
-
-
-
   // Start by pulsing the reset low for some nanoseconds
   initial begin
     rst_async_n = 1'b0;
      cm0_a <= 0;
      cm0_b <= 0;
 
+     mut0_r1 <= 1'b0;
+     mut0_r2 <= 1'b0;
+
+
     #5;
     rst_async_n = 1'b1;
     $display("-I- Reset is released");
-
+     // Muller C-Element
      #10;
      cm0_a <= 0;
      cm0_b <= 0;
@@ -60,6 +68,33 @@ module tb;
      cm0_b <= 0;
 
      #10;
+
+     // Mutex
+     mut0_r1 <= 1'b1;
+     mut0_r2 <= 1'b0;
+     #10;
+     mut0_r1 <= 1'b0;
+     mut0_r2 <= 1'b0;
+     #10;
+     mut0_r1 <= 1'b0;
+     mut0_r2 <= 1'b1;
+     #10;
+     mut0_r1 <= 1'b0;
+     mut0_r2 <= 1'b0;
+     #10;
+     mut0_r1 <= 1'b1;
+     mut0_r2 <= 1'b1;
+     #10;
+     mut0_r1 <= 1'b0;
+     mut0_r2 <= 1'b1;
+     #10;
+     mut0_r1 <= 1'b0;
+     mut0_r2 <= 1'b0;
+     #10;
+
+
+
+
 
     $display("-I- Done !");
 
